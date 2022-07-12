3f69ce871ef05f98ae76fcae0e95b58d225b91bf
Hernan Garat <hgarat@w3itsolutions.net>
2022-07-11 22:39:24 -0300

    changes

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 var express = require("express");
-const { engine } = require("express-handlebars");
+const cors = require("cors");
+const app = express();
+const http = require("http");
+const { randomBytes, randomInt } = require("crypto");
+const server = http.createServer(app);
 
-var app = express();
-
-app.engine(
-  "handlebars",
-  engine({ extname: ".handlebars", defaultLayout: "main" })
-);
-
-app.set("view engine", "handlebars");
-
-app.use('/', express.static('public'));
-
-app.get("/", function (req, res) {
-  res.render("home", { sky: "#ECECEC" });
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
 });
 
-app.listen(3000);
+app.use(cors());
+
+app.use("/", express.static("public"));
+
+app.get("/map", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.broadcast.emit("newGuest", { position: randomInt(60) });
+
+
+  socket.on('move', (msg) => {
+    io.emit('move', msg);
+  });
+
+});
+
+server.listen(3000);
